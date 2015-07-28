77bce7d6aba4da5cfe7a4df6eafefee9e03f41ef
Justin DeLuna <jsrdeluna@gmail.com>
2015-07-28 13:03:42 -0700

    Push db data to frontend

--- twitter.js
+++ twitter.js
@@ -3,16 +3,14 @@ var app = express();
 
 var Twitter = require('twitter');
 var mongo = require('mongodb');
-//var mongoose = require('mongoose');
 
 
 var client = new Twitter ({
-  consumer_key: 'AOztrA5dsNyCNER0g89k0slQi',
-  consumer_secret: 'kxmTIpyvgr9hJq1KkzFE9E2noVZ9VDALMz8qKFOudAJipBEEbW',
-  access_token_key: '16349436-U5gvCtG0Qk23j5JeJOpXy4AJtSSp7xuDmuCinJKpq',
-  access_token_secret: 'IQpkWFvNM8zB8x0Q2O82BJLTYzoqeMjCJUu3aN3HVEKny'
+  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
+  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
+  access_token_key: 'process.env.TWITTER_ACCESS_TOKEN_KEY',
+  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET'
 });
-
 
 mongo.connect('mongodb://localhost/27017', function(err, db) {
   if (err) {
@@ -26,21 +24,28 @@ mongo.connect('mongodb://localhost/27017', function(err, db) {
   client.stream('statuses/filter', {locations: '-122.41, 47.54, -122.24, 47.70'}, function(stream) {
     console.log('Stream has started...\n');
     stream.on('data', function(tweet) {
-
       if (tweet.geo) {
         col.insert({curDate: new Date().toISOString(), text: tweet.text, latitute: tweet.geo.coordinates[1],longitude: tweet.geo.coordinates[0]});
         console.log(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
       }
-      
     });
     
     stream.on('error', function(error) {
       console.error(error);
     });
 
+    app.get('/', function(req, res) {
+      col.find().toArray(function(err, result) {
+        res.json(result);
+      });
+    });
+
     stream.on('end', console.log.bind(console));
   });
 });
+
+
+
 
 
 var port = process.env.PORT || 3000;
