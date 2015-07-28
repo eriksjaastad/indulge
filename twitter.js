3640c5d99065393e01873242b8a3c0c37481739d
Erik Sjaastad <spudlogic@gmail.com>
2015-07-28 13:02:16 -0700

    got rid of unused files

--- twitter.js
+++ twitter.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var app = express();
-
-var Twitter = require('twitter');
+var app = require('express')();
 var mongo = require('mongodb');
-var client = new Twitter ({
-  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
-  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
-  access_token_key: 'process.env.TWITTER_ACCESS_TOKEN_KEY',
-  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET'
-});
+var Twitter = require('twitter');
 
+var client = new Twitter ({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 mongo.connect('mongodb://localhost/27017', function(err, db) {
   if (err) {
@@ -23,12 +21,10 @@ mongo.connect('mongodb://localhost/27017', function(err, db) {
   client.stream('statuses/filter', {locations: '-122.41, 47.54, -122.24, 47.70'}, function(stream) {
     console.log('Stream has started...\n');
     stream.on('data', function(tweet) {
-
       if (tweet.geo) {
         col.insert({curDate: new Date().toISOString(), text: tweet.text, latitute: tweet.geo.coordinates[1],longitude: tweet.geo.coordinates[0]});
         console.log(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
       }
-
     });
 
     stream.on('error', function(error) {
