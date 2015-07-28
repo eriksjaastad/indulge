9838c5f7bc9f4ac2e5d42e3c769601c9841073c2
Erik Sjaastad <spudlogic@gmail.com>
2015-07-28 03:40:29 -0700

    setting up database and formatting twitter feed

--- twitter.js
+++ twitter.js
@@ -1,18 +1,25 @@
 var Twitter = require('twitter');
 var client = new Twitter ({
-  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
-  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
-  access_token_key: 'process.env.TWITTER_ACCESS_TOKEN_KEY',
-  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
 
-client.stream('statuses/filter', {track: 'Seattle'}, function(stream) {
+client.stream('statuses/filter', {locations: '-122.41, 47.54, -122.24, 47.70'}, function(stream) {
   console.log("stream opened");
 
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+    var now = new Date();
+    if(!tweet.geo) {return}
+    else {
+      //console.log(now.format('dd/M/yy h:mm tt'));
+      console.log(tweet.text);
+      console.log(tweet.geo.coordinates[0]);
+      console.log(tweet.geo.coordinates[1]);
+    }
   });
 
   stream.on('error', function(error) {
