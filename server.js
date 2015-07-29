545c29384942212204a472bb089ef616378d9f8d
Justin DeLuna <jsrdeluna@gmail.com>
2015-07-29 13:53:30 -0700

    ENV Vars froms strings to vars

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var io = require('socket.io')(http);
 var Twitter = require('twitter');
 
 var client = new Twitter ({
-  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
-  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
-  access_token_key: 'process.env.TWITTER_ACCESS_TOKEN_KEY',
-  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 app.use(express.static(__dirname + '/public'));
