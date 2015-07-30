4f5d6cf2dd5f83fc7f894dffa2447bcb87ff4abc
Luke Gabrieli <usetheforce10@gmail.com>
2015-07-30 11:54:22 -0700

    add basic stylins

--- app/js/controllers/mapController.js
+++ app/js/controllers/mapController.js
@@ -11,10 +11,80 @@ module.exports = function(app) {
 
       // -----Make Google Map-----
 
+      // Map Styling
+
+      var stylesArray = [
+        {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry.fill",
+          "stylers": [{"color": "#e9e5dc"}]
+        },
+        {
+          "featureType": "landscape.natural",
+          "elementType": "geometry.fill",
+          "stylers": [{"visibility": "on"},{"color": "#b8cb93"}]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.business",
+          "elementType": "all",
+          "stylers": [{"visibility": "simplified"}]
+        },
+        {
+          "featureType": "poi.medical",
+          "elementType": "all",
+          "stylers": [{"visibility": "on"}]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "all",
+          "stylers": [{"visibility": "on"}]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [{"color": "#ccdca1"}]
+        },
+        {
+          "featureType": "poi.sports_complex",
+          "elementType": "all",
+          "stylers": [{"visibility": "on"}]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [{"hue": "#ff0000"}, {"saturation": -100}, {"lightness": 99}]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [{"color": "#808080"}, {"lightness": 54}, {"visibility": "off"}]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#767676"}]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.stroke",
+          "stylers": [{"color": "#ffffff"}]
+        },
+        {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [{"saturation": 43},{"lightness": -11},{"color": "#89cada"}]
+        }
+      ]
+
       var mapOptions = {
         zoom: 12,
         center: new google.maps.LatLng(47.623581, -122.335661),
-        // styles: stylesArray,
+        styles: stylesArray,
         disableDefaultUI: true,
         zoomControl: true,
         zoomControlOptions: {
@@ -32,26 +102,26 @@ module.exports = function(app) {
       var infoWindow = new google.maps.InfoWindow();
       var createMarker = function (info){
         var marker = new google.maps.Marker({
-            map: $scope.map,
-            position: new google.maps.LatLng(info.longitude, info.latitute),
-            title: info.text
+          map: $scope.map,
+          position: new google.maps.LatLng(info.longitude, info.latitute),
+          title: info.text
         });
 
         google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>');
-            infoWindow.open($scope.map, marker);
+          infoWindow.setContent('<h2>' + marker.title + '</h2>');
+          infoWindow.open($scope.map, marker);
         });
 
         $scope.markers.push(marker);
       }
 
       for (var i = 0; i < lukesData.length; i++){
-          createMarker(lukesData[i]);
+        createMarker(lukesData[i]);
       }
 
       $scope.openInfoWindow = function(e, selectedMarker){
-          e.preventDefault();
-          google.maps.event.trigger(selectedMarker, 'click');
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
       }
 
       $scope.$apply();
