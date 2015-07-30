7fc19aa349f3706f385c52a07522e1e2fb684e0b
Erik Sjaastad <spudlogic@gmail.com>
2015-07-30 12:05:15 -0700

    removed local http for socket.io

--- app/js/controllers/mapController.js
+++ app/js/controllers/mapController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var socket = io.connect(process.env.SOCKET_IO_CONNECTION || 'http://127.0.0.1:3000');
+var socket = io.connect(process.env.SOCKET_IO_CONNECTION);
 module.exports = function(app) {
   app.controller('mapController', ['$scope', function($scope){
 
