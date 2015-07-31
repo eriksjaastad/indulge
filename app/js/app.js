e86dc5f8100df1d5a1d4b76c9573fcc939aadb60
Luke Gabrieli <usetheforce10@gmail.com>
2015-07-30 22:39:16 -0700

    add some directives

--- app/js/app.js
+++ app/js/app.js
@@ -6,3 +6,7 @@ var indulgeApp = angular.module('indulgeApp',[]);
 
 // require the controller
 require('./controllers/mapController.js')(indulgeApp);
+
+// require the directive
+require('./directives/aboutDirective.js')(indulgeApp);
+require('./directives/tweetsDirective.js')(indulgeApp);
